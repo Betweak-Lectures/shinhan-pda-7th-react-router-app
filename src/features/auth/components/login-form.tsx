80b1dd08d5de57b2fb534692e535c5e9788d64a4
ys <ys@betweak.com>
2026-02-23 14:24:35 +0900

    Apply react-hook-form to login-form

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Field,
   FieldDescription,
+  FieldError,
   FieldGroup,
   FieldLabel,
 } from "@/components/ui/field";
@@ -14,34 +15,28 @@ import { Link, useNavigate } from "react-router";
 import { useAuth } from "../hooks";
 import { toast } from "sonner";
 
+import { Controller, useForm, type SubmitHandler } from "react-hook-form";
+import {
+  loginFormSchema,
+  type LoginFormSchema,
+  type SignupFormSchema,
+} from "../schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form";
+
 export function LoginForm() {
-  const [formInput, setFormInput] = useState<LoginFormData>({
-    email: "",
-    password: "",
+  const form = useForm<LoginFormSchema>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onChange",
   });
-  const navigate = useNavigate();
-  const { login, user } = useAuth();
 
-  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setFormInput((prev) => {
-      return {
-        ...prev,
-        [e.target.name]: e.target.value,
-      };
-    });
+  const onSubmit: SubmitHandler<LoginFormSchema> = (data) => {
+    console.log(data);
   };
-
-  const onSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const result = await login(formInput);
-    if (result.success) {
-      toast(`${result.data.user.nickname}님 환영합니다.`);
-      navigate("/");
-    } else {
-      toast.error(`${result.error.message}`);
-    }
-  };
-
   return (
     <div className={"flex flex-col gap-6"}>
       <Card>
@@ -49,42 +44,49 @@ export function LoginForm() {
           <CardTitle>로그인</CardTitle>
         </CardHeader>
         <CardContent>
-          <form onSubmit={onSubmit}>
-            <FieldGroup>
-              <Field>
-                <FieldLabel htmlFor="email">이메일</FieldLabel>
-                <Input
-                  id="email"
-                  type="email"
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FieldGroup>
+                <Controller
                   name="email"
-                  placeholder="m@example.com"
-                  value={formInput.email}
-                  onChange={onInputChange}
-                  required
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field data-invalid={fieldState.invalid}>
+                      <FieldLabel htmlFor={field.name}>이메일</FieldLabel>
+                      <Input {...field} id={field.name} type="email" />
+                      {fieldState.invalid && (
+                        <FieldError errors={[fieldState.error]} />
+                      )}
+                    </Field>
+                  )}
                 />
-              </Field>
-              <Field>
-                <div className="flex items-center">
-                  <FieldLabel htmlFor="password">비밀번호</FieldLabel>
-                </div>
-                <Input
-                  id="password"
-                  type="password"
+                <Controller
                   name="password"
-                  required
-                  value={formInput.password}
-                  onChange={onInputChange}
-                />
-              </Field>
-              <Field>
-                <Button type="submit">로그인</Button>
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field data-invalid={fieldState.invalid}>
+                      <FieldLabel htmlFor={field.name}>비밀번호</FieldLabel>
 
-                <FieldDescription className="text-center">
-                  아직 계정이 없으십니까? <Link to="/signup">회원가입</Link>
-                </FieldDescription>
-              </Field>
-            </FieldGroup>
-          </form>
+                      <Input {...field} id={field.name} type="password" />
+                      {fieldState.invalid && (
+                        <FieldError errors={[fieldState.error]} />
+                      )}
+                    </Field>
+                  )}
+                />
+
+                <Field>
+                  <Button type="submit" disabled={form.formState.isSubmitting}>
+                    로그인
+                  </Button>
+
+                  <FieldDescription className="text-center">
+                    아직 계정이 없으십니까? <Link to="/signup">회원가입</Link>
+                  </FieldDescription>
+                </Field>
+              </FieldGroup>
+            </form>
+          </Form>
         </CardContent>
       </Card>
     </div>
